dcab92fc0548efa8c952264206eb3adb89be4d02
Jihyun3478 <leejihyun3478@gmail.com>
2025-10-31 17:02:22 +0900

    refactor: 변수명 클래스명과 일관되도록 수정

--- src/main/java/lotto/PurchaseAmount.java
+++ src/main/java/lotto/PurchaseAmount.java
@@ -1,33 +1,33 @@
 package lotto;
 
 public class PurchaseAmount {
-    private int money;
+    private int purchaseAmount;
 
-    public PurchaseAmount(int money) {
-        validate(money);
-        this.money = money;
+    public PurchaseAmount(int purchaseAmount) {
+        validate(purchaseAmount);
+        this.purchaseAmount = purchaseAmount;
     }
 
-    private void validate(int money) {
-        validateUnit(money);
-        validateLowerLimit(money);
-        validateUpperLimit(money);
+    private void validate(int purchaseAmount) {
+        validateUnit(purchaseAmount);
+        validateLowerLimit(purchaseAmount);
+        validateUpperLimit(purchaseAmount);
     }
 
-    private void validateUnit(int money) {
-        if (money % 1000 != 0) {
+    private void validateUnit(int purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
         }
     }
 
-    private void validateLowerLimit(int money) {
-        if (money < 1000) {
+    private void validateLowerLimit(int purchaseAmount) {
+        if (purchaseAmount < 1000) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 이상이여야 합니다.");
         }
     }
 
-    private void validateUpperLimit(int money) {
-        if (money > 100000) {
+    private void validateUpperLimit(int purchaseAmount) {
+        if (purchaseAmount > 100000) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 100,000원을 초과할 수 없습니다.");
         }
     }
