b2431cc23c90b717d2e2d2134f629d40d185e0de
Jihyun3478 <leejihyun3478@gmail.com>
2025-10-31 18:42:19 +0900

    test: 보너스 번호 테스트 케이스 추가
    보너스 번호 테스트 케이스 추가
    
    - 보너스 번호가 1과 45 사이의 숫자가 아닌 경우, 예외를 발생시킨다.

--- src/test/java/lotto/BonusNumberTest.java
+++ src/test/java/lotto/BonusNumberTest.java
@@ -7,26 +7,10 @@ import org.junit.jupiter.api.Test;
 
 public class BonusNumberTest {
     @Test
-    @DisplayName("당첨 번호가 6개가 아닌 경우 예외가 발생한다.")
-    void 당첨_번호가_6개가_아닌_경우_예외가_발생한다() {
-        assertThatThrownBy(() -> new BonusNumber())
+    @DisplayName("보너스 번호가 1과 45 사이의 숫자가 아닌 경우 예외가 발생한다.")
+    void 보너스_번호가_1과_45사이의_숫자가_아닌_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> new BonusNumber(0))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 당첨 번호는 6개여야 합니다.");
-    }
-
-    @Test
-    @DisplayName("당첨 번호가 1과 45 사이의 숫자가 아닌 경우 예외가 발생한다.")
-    void 당첨_번호가_1과_45사이의_숫자가_아닌_경우_예외가_발생한다() {
-        assertThatThrownBy(() -> new BonusNumber())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 당첨 번호는 1과 45 사이의 숫자여야 합니다.");
-    }
-
-    @Test
-    @DisplayName("당첨 번호가 중복된 경우 예외가 발생한다.")
-    void 당첨_번호가_중복된_경우_예외가_발생한다() {
-        assertThatThrownBy(() -> new BonusNumber())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 당첨 번호는 중복되지 않아야 합니다.");
+                .hasMessage("[ERROR] 보너스스 번호는 1과 45 사이의 숫자여야 합니다.");
     }
 }
