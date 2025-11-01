3d3d3a03792617211bb5dd2a28a91b64c6e4db8e
Jihyun3478 <leejihyun3478@gmail.com>
2025-11-01 18:00:56 +0900

    refactor: InputValidator 클래스 util 패키지 하위로 이동

--- src/test/java/lotto/InputValidatorTest.java
+++ src/test/java/lotto/InputValidatorTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import lotto.util.InputValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
