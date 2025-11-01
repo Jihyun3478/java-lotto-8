ee3e0e7bcf698df9ecc797024d6176af65df7e1e
Jihyun3478 <leejihyun3478@gmail.com>
2025-11-01 17:04:15 +0900

    refactor: Lotto 클래스 내 변수명 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,38 +3,50 @@ package lotto;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private final List<Integer> lotto;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+    public Lotto(List<Integer> lotto) {
+        validate(lotto);
+        this.lotto = lotto;
     }
 
-    private void validate(List<Integer> numbers) {
-        validateLottoSize(numbers);
-        validateDuplicateNumber(numbers);
+    private void validate(List<Integer> lotto) {
+        validateLottoSize(lotto);
+        validateDuplicateNumber(lotto);
     }
 
-    private void validateLottoSize(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+    private void validateLottoSize(List<Integer> lotto) {
+        if (lotto.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    private void validateDuplicateNumber(List<Integer> numbers) {
-        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
-        if (numbers.size() != uniqueNumbers.size()) {
+    private void validateDuplicateNumber(List<Integer> lotto) {
+        Set<Integer> uniqueNumbers = new HashSet<>(lotto);
+        if (lotto.size() != uniqueNumbers.size()) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
         }
     }
 
     public int size() {
-        return numbers.size();
+        return lotto.size();
     }
 
-    public List<Integer> getNumbers() {
-        return numbers;
+    public int getMatchCount(List<Integer> winningNumber) {
+        return Math.toIntExact(lotto.stream()
+                .filter(number -> winningNumber.stream()
+                        .anyMatch(Predicate.isEqual(number)))
+                .count());
+    }
+
+    public boolean isBonusMatched(int bonusNumber) {
+        return lotto.contains(bonusNumber);
+    }
+
+    public List<Integer> getLotto() {
+        return lotto;
     }
 }
