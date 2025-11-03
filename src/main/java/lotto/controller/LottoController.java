856ab617eea83922994be8bf217de172d3584427
Jihyun3478 <leejihyun3478@gmail.com>
2025-11-03 11:51:05 +0900

    fix: StatisticsService 삭제
    StatisticsService 삭제
    
    - 기존 StatisticsService 클래스 내에 있던 calculateWinningStatistics 메서드 -> LottoService로 이동
    - LottoController의 인스턴스 4개 -> 3개로 감소
    - 현재 서비스 로직에서 담당하고 있는 역할이 적은 관계로 과한 서비스 분리라고 판단함

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -8,20 +8,16 @@ import lotto.model.domain.PurchaseAmount;
 import lotto.model.domain.WinningNumber;
 import lotto.model.domain.operation.WinningStatistics;
 import lotto.model.service.LottoService;
-import lotto.model.service.StatisticsService;
 
 public class LottoController {
     private final InputHandler inputHandler;
     private final OutputHandler outputHandler;
     private final LottoService lottoService;
-    private final StatisticsService statisticsService;
 
-    public LottoController(InputHandler inputHandler, OutputHandler outputHandler, LottoService lottoService,
-                           StatisticsService statisticsService) {
+    public LottoController(InputHandler inputHandler, OutputHandler outputHandler, LottoService lottoService) {
         this.inputHandler = inputHandler;
         this.outputHandler = outputHandler;
         this.lottoService = lottoService;
-        this.statisticsService = statisticsService;
     }
 
     public void start() {
@@ -36,7 +32,7 @@ public class LottoController {
         WinningNumber winningNumber = inputHandler.handleWinningNumber();
         BonusNumber bonusNumber = inputHandler.handleBonusNumber(winningNumber);
 
-        WinningStatistics winningStatistics = statisticsService.calculateWinningStatistics(lottos, winningNumber,
+        WinningStatistics winningStatistics = lottoService.calculateWinningStatistics(lottos, winningNumber,
                 bonusNumber);
 
         outputHandler.displayWinningStatistics(winningStatistics, purchaseAmount);
