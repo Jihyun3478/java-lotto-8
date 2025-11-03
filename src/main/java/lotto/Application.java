f4c0c758254c2f9997908c74cdd456a651c0112f
Jihyun3478 <leejihyun3478@gmail.com>
2025-11-03 22:24:34 +0900

    feat: 애플리케이션이 종료될 때, 자원 해제 명시

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,17 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import lotto.common.config.AppConfig;
 import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        LottoController lottoController = appConfig.lottoController();
-        lottoController.start();
+        try {
+            AppConfig appConfig = new AppConfig();
+            LottoController lottoController = appConfig.lottoController();
+            lottoController.start();
+        } finally {
+            Console.close();
+        }
     }
 }
