16614584d0ee25fc8b5becf10b81615ba2a2d022
Jihyun3478 <leejihyun3478@gmail.com>
2025-11-03 21:48:26 +0900

    test: BonusNumberTest 테스트 케이스 추가
    BonusNumberTest 테스트 케이스 추가
    
    - 보너스 번호 생성 테스트
      - 보너스 번호를 생성한다.
    - 보너스 번호 예외 테스트
      - 보너스 번호가 당첨 번호와 중복될 경우 예외가 발생한다.
     - 보너스 번호가 1과 45 사이의 숫자가 아닌 경우 예외가 발생한다.

--- src/test/java/lotto/model/domain/BonusNumberTest.java
+++ src/test/java/lotto/model/domain/BonusNumberTest.java
@@ -4,30 +4,75 @@ import static lotto.common.constant.CommonConstant.MAXIMUM_NUMBER_RANGE;
 import static lotto.common.constant.CommonConstant.MINIMUM_NUMBER_RANGE;
 import static lotto.common.exception.ErrorMessage.BONUS_NUMBER_INVALID_RANGE;
 import static lotto.common.exception.ErrorMessage.BONUS_NUMBER_MUST_NOT_DUPLICATE;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class BonusNumberTest {
-    @Test
-    @DisplayName("보너스 번호가 당첨 번호와 중복될 경우 예외가 발생한다.")
-    void 보너스_번호가_당첨_번호와_중복될_경우_예외가_발생한다() {
-        WinningNumber winningNumber = new WinningNumber(List.of(1,2,3,4,5,6));
+    @Nested
+    @DisplayName("보너스 번호 생성 테스트")
+    class 보너스_번호_생성_테스트 {
+        @Test
+        @DisplayName("보너스 번호를 생성한다.")
+        void 보너스_번호를_생성한다() {
+            WinningNumber winningNumber = new WinningNumber(List.of(1, 2, 3, 4, 5, 6));
+            BonusNumber bonusNumber = new BonusNumber(7, winningNumber);
 
-        assertThatThrownBy(() -> new BonusNumber(1, winningNumber))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(BONUS_NUMBER_MUST_NOT_DUPLICATE.getMessage());
+            assertThat(bonusNumber.getBonusNumber()).isEqualTo(7);
+        }
     }
 
-    @Test
-    @DisplayName("보너스 번호가 1과 45 사이의 숫자가 아닌 경우 예외가 발생한다.")
-    void 보너스_번호가_1과_45_사이의_숫자가_아닌_경우_예외가_발생한다() {
-        WinningNumber winningNumber = new WinningNumber(List.of(1,2,3,4,5,6));
+    @Nested
+    @DisplayName("보너스 번호 예외 테스트")
+    class 보너스_번호_예외_테스트 {
+        @ParameterizedTest
+        @MethodSource("generateDuplicatesWithWinningNumber")
+        @DisplayName("보너스 번호가 당첨 번호와 중복될 경우 예외가 발생한다.")
+        void 보너스_번호가_당첨_번호와_중복될_경우_예외가_발생한다(List<Integer> winningNumbers, int bonusNumber) {
+            WinningNumber winningNumber = new WinningNumber(winningNumbers);
 
-        assertThatThrownBy(() -> new BonusNumber(0, winningNumber))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(BONUS_NUMBER_INVALID_RANGE.getMessage(MINIMUM_NUMBER_RANGE, MAXIMUM_NUMBER_RANGE));
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> new BonusNumber(bonusNumber, winningNumber))
+                    .withMessage(BONUS_NUMBER_MUST_NOT_DUPLICATE.getMessage());
+        }
+
+        @ParameterizedTest
+        @MethodSource("generateInvalidRange")
+        @DisplayName("보너스 번호가 1과 45 사이의 숫자가 아닌 경우 예외가 발생한다.")
+        void 보너스_번호가_1과_45_사이의_숫자가_아닌_경우_예외가_발생한다(int invalidNumber) {
+            WinningNumber winningNumber = new WinningNumber(List.of(1, 2, 3, 4, 5, 6));
+
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> new BonusNumber(invalidNumber, winningNumber))
+                    .withMessage(BONUS_NUMBER_INVALID_RANGE.getMessage(MINIMUM_NUMBER_RANGE, MAXIMUM_NUMBER_RANGE));
+        }
+
+        private static Stream<Arguments> generateDuplicatesWithWinningNumber() {
+            return Stream.of(
+                    Arguments.of(List.of(1, 2, 3, 4, 5, 6), 1),
+                    Arguments.of(List.of(1, 2, 3, 4, 5, 6), 6),
+                    Arguments.of(List.of(10, 20, 30, 40, 41, 42), 20),
+                    Arguments.of(List.of(10, 20, 30, 40, 41, 42), 42)
+            );
+        }
+
+        private static Stream<Arguments> generateInvalidRange() {
+            return Stream.of(
+                    Arguments.of(0),
+                    Arguments.of(-1),
+                    Arguments.of(46),
+                    Arguments.of(50),
+                    Arguments.of(100),
+                    Arguments.of(-100)
+            );
+        }
     }
 }
